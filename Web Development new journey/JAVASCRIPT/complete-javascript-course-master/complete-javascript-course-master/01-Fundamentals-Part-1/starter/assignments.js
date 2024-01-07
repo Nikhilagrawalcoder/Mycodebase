58b0091c9afda4a0de097d9efd9ee017f22d4348
NIK0605 <nikhilagrawal0605@gmail.com>
2024-01-08 00:03:01 +0530

    addd

---
+++ Web Development new journey/JAVASCRIPT/complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,12 @@
+let country='India';
+let continent='Asia';
+let population=1400000000;
+console.log(typeof(country));
+console.log(continent);
+console.log(typeof(population));
+
+
+let isisland=true;
+let language;
+console.log(typeof(isisland));
+console.log(language);
---
+++ Web Development new journey/JAVASCRIPT/complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,6 @@
+// let js="amazing";
+// console.log(40+20+8-9);
+// console.log("jonas");
+// console.log("23");
+let age=30;
+ age=31;
